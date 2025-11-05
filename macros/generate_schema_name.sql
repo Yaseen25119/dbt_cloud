8f132ee51bbefb6dd3315809e55ecc51044209e8
yaseenbasha2001@gmail.com <yaseenbasha2001@gmail.com>
2025-11-05 15:33:52 +0000

    modified the macro

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,8 +1,3 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-        {{default_schema}}
-    {%- else -%}
-        {{custom_schema_name | trim}}
-    {%- endif -%}
+    {{ custom_schema_name or target.schema }}
 {%- endmacro %}
